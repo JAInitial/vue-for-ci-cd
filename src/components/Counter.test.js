f38174980e1f9a3916596d1e79dbb519ebd2a5b1
Anna <>
2021-09-16 15:56:24 +0800

    add: test

---
+++ src/components/Counter.test.js
@@ -0,0 +1,24 @@
+  
+import { render, fireEvent } from '@testing-library/vue';
+
+//import Counter from './Counter.vue';
+import Counter from ""
+
+test('Original title is 「當前點擊次數0」', () => {
+   const { getByTestId } = render(Counter);
+
+//   const title = getByTestId('currentCounter');
+
+//   expect(title.innerHTML).toBe('當前點擊次數0');
+expect(1).toBe(1);
+});
+
+/* test('After I click button the title will change to 「當前點擊次數1」', async () => {
+  const { getByTestId, getByText } = render(Counter);
+
+  const incrementButton = getByText('點我加1');
+  await fireEvent.click(incrementButton);
+
+  const title = getByTestId('currentCounter');
+  expect(title.innerHTML).toBe('當前點擊次數1');
+}); */
---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1 data-testId="currentCounter">當前點擊次數{{ count }}</h1>
+    <button @click="increment">點我加1</button>
+    <!-- <button @click="increment">點我減1</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+};
+</script>
